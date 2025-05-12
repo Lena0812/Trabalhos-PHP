f82ba7452e66e77aee2815fe90652bdda9b1b97f
Lena <ylena.mrse@gmail.com>
2025-05-12 10:18:03 -0300

    Primeiro código PHP
    <h1>Minha primeira aula de PHP</h1>
    
    <?php
    //Exibindo uma mensagem na tela
    echo "Olá,mundo!";
    $nome = "Ylena Maria";//string
    $idade = 18; //inteiro 
    $altura = 1.64;//
    $peso = 62.00; // float 
    $casado = False;//booleano (true or false)
    $Filhos = null;//Nulo(sem valor definido)
    
    //Exibindo o valor de uma variável
    echo "Nome: $nome <br>";
    echo "idade: $idade <br>";
    echo "Altura: $altura <br>";
    echo "peso: $peso <br>";
    
    ?>

---
+++ oi.php
@@ -0,0 +1,19 @@
+<h1>Minha primeira aula de PHP</h1>
+
+<?php
+//Exibindo uma mensagem na tela
+echo "Olá,mundo!";
+$nome = "Ylena Maria";//string
+$idade = 18; //inteiro 
+$altura = 1.64;//
+$peso = 62.00; // float 
+$casado = False;//booleano (true or false)
+$Filhos = null;//Nulo(sem valor definido)
+
+//Exibindo o valor de uma variável
+echo "Nome: $nome <br>";
+echo "idade: $idade <br>";
+echo "Altura: $altura <br>";
+echo "peso: $peso <br>";
+
+?>
